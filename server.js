33fa150aca9180036f11a7d4048be71b73b9c0e3
Monineath Heng <neath@neaths-mac.local>
2022-04-16 12:11:42 -0400

    a9 MONGO sp22

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ import helloController from "./controllers/hello-controller.js";
 import userController from "./controllers/user-controller.js";
 import tuitsController from "./controllers/tuits-controller.js";
 import mongoose from 'mongoose'
-const DB_CONNECTION_STRING = 'mongodb+srv://wEbDev:supersecretpassword@cluster0.hnwky.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/webdev';
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb+srv://wEbDev:supersecretpassword@cluster0.hnwky.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+|| 'mongodb://localhost:27017/webdev';
 mongoose.connect(CONNECTION_STRING);
 
 const app = express();
